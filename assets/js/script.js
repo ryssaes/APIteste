46cd9f724427d2576930810fd5958f887f6b0765
Ryssa Teles Esmeraldo Sobreira <dig.ryssasobreira@opovo.local>
2023-12-11 15:29:59 -0300

    feat: despesas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var dep;
 var detalhe;
+var despesas;
+var profissao;
 
 
 function apiBase() {
@@ -35,7 +37,6 @@ function filterCountry(e) {
 function createDeputados() {
     let div = document.getElementById('deputados-box')
     dep.forEach(element => {
-        console.log(dep)
         let div_container = ''
         div_container = div.innerHTML
         let html = `<div class="box-dep" data-id="${element.id}">
@@ -55,7 +56,6 @@ function getperfil() {
     $('.box-dep').click(function () {
         let getid = $(this).attr('data-id');
         let url = 'https://dadosabertos.camara.leg.br/api/v2/deputados/' + getid
-        console.log(url)
         $.ajax({
             url: url,
             type: 'GET',
@@ -65,13 +65,17 @@ function getperfil() {
                 let dado = response.dados
                 detalhe = dado;
                 createperfil();
+                closeBox();
+                getDespesas();
+                createDespesas();
             })
     })
 }
 
 
-function createperfil(){
+function createperfil() {
     let div = document.getElementById('perfil-box');
+    div.innerHTML = '';
     let div_container = ''
 
     let regxcpf = /(\d{3})(\d{3})(\d{3})(\d{2})/;
@@ -91,6 +95,7 @@ function createperfil(){
 
     div_container = div.innerHTML
     let html = `<div class="box-perfil">
+    <span class="btn-close" id="btn-close"></span>
     <figure>
     <img src="${detalhe.ultimoStatus.urlFoto}"></figure>
     <span>${detalhe.nomeCivil}</span>
@@ -98,6 +103,72 @@ function createperfil(){
     <span class="data">Data:${detalhe.data = 'undefined' ? 'Não preenchido' : detalhe.data}</span>
     <span class="escolaridade">Escolaridade:${detalhe.escolaridade}</span>
     <span class="cpf">CPF:${cpftratado}</span>
-    <span>${datamontada}</span></div>`
+    <span>Data de nascimento: ${datamontada}</span></div>
+    <h1 class="title-section">Despesas</h1>
+    <div class="despesas" id="despesas"></div>`
     div.innerHTML = div_container + html
+}
+
+function getDespesas() {
+    $('.box-dep').click(function () {
+        let getid = $(this).attr('data-id')
+        let url = `https://dadosabertos.camara.leg.br/api/v2/deputados/${getid}/despesas?itens=10&ordem=ASC&ordenarPor=ano`
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json'
+        })
+            .done(function (response) {
+                let dado = response.dados
+                despesas = dado;
+            })
+    })
+}
+
+function createDespesas() {
+    let div = document.getElementById('despesas')
+    let div_container = ''
+    let regx = /[^a-zA-Z0-9]/g; //Regx Remover Espaçoes
+    let regdata = /(\d{4})(\d{2})(\d{2})/;
+
+    despesas.forEach(function (index) {
+        let datanascimento = index.dataDocumento;
+        let tratardatanascimento = datanascimento.replace(regx, "");
+        let secondatanascimento = tratardatanascimento.match(regdata);
+        let datamontada = secondatanascimento[3] + '/' + secondatanascimento[2] + '/' + secondatanascimento[1];
+        div_container = div.innerHTML
+        let html = `<div class="box-despesas">
+        <h3 class="valor">${index.valorDocumento.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<h3>
+        <p class="tipo-despesas">${index.tipoDespesa}</p>
+        <span class="data-despesa">${datamontada}</span>
+        </div>`
+        div.innerHTML = div_container + html
+    })
+}
+
+function getProfissao(){
+    $('.box-dep').click(function(){
+        let getid = $(this).attr('data-id')
+        let url = `https://dadosabertos.camara.leg.br/api/v2/deputados/${getid}/profissoes`
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json'
+        })
+            .done(function (response) {
+                let dado = response.dados
+                profissao = dado;
+                console.log(profissao)
+            })
+    })
+}
+
+function closeBox() {
+    $('#btn-close').click(function () {
+        $('.box-perfil').remove();
+        $('.box-despesas').remove();
+        $('.title-section').remove();
+    })
 }
